e01abac68d0c27ed81e8f54eb20672975ecac9ae
rodrigordgfs <rodrigordgfs@gmail.com>
2025-06-03 00:40:12 -0300

    feature: Ajuste no prisma

--- src/libs/prisma.js
+++ src/libs/prisma.js
@@ -1,8 +1,19 @@
 import pkg from "@prisma/client";
 const { PrismaClient } = pkg;
 
-const prisma = new PrismaClient({
-  log: ["query"],
-});
+let prisma;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient({
+    log: ["query", "error", "warn", "info"],
+  });
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient({
+      log: ["query"],
+    });
+  }
+  prisma = global.prisma;
+}
 
 export default prisma;
